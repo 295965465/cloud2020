12381dabf343567d0536a26c40eac3e5beadba45
295965465@qq.com <wo295965465>
2020-04-07 11:49:20 +0800

    ribbon

--- cloud-comsumer-order80/src/main/java/com/springcloud/controller/OrderController.java
+++ cloud-comsumer-order80/src/main/java/com/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.springcloud.controller;
 import com.springcloud.entities.CommontResult;
 import com.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,5 +31,20 @@ public class OrderController {
     @GetMapping("/cousumer/payment/get/{id}")
     public CommontResult<Payment> getPayment(@PathVariable("id") Long id) {
         return RestTemplate.getForObject(urls + "/payment/get/"+id, CommontResult.class);
+    }
+
+
+    @GetMapping("/cousumer/payment/getForEntity/{id}")
+    public CommontResult<Payment> getForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommontResult> forEntity = RestTemplate.getForEntity(urls + "/payment/get/" + id, CommontResult.class);
+        log.info(forEntity.getStatusCodeValue()+"a");
+        if (forEntity.getBody().getCode().equals(200)&&forEntity.getStatusCode().is2xxSuccessful()){
+
+            return forEntity.getBody() ;
+        }else{
+            return new CommontResult<>(404,"查询失败",null);
+
+        }
+
     }
 }
