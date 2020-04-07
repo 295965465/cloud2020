5d55b4bc79228df2bef00cc96f43f1759196c5d6
295965465@qq.com <wo295965465>
2020-04-07 17:57:55 +0800

    openfeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/springcloud/OrderFeignMain80.java
@@ -0,0 +1,16 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: DELL
+ * @Date: 2020/4/7 17:44
+ * @Description:
+ */
+@SpringBootApplication
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
